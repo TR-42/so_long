b67614f658a6c3deebe331892c8c350f1d8714f8
kfujita <kfujita@student.42tokyo.jp>
2023-04-09 00:46:34 +0900

    Create error_messages.h

---
+++ headers/error_messages.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_messages.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/09 00:02:55 by kfujita           #+#    #+#             */
+/*   Updated: 2023/04/09 00:42:56 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_MESSAGES_H
+# define ERROR_MESSAGES_H
+
+static const char	g_err_inval[]
+	= "Invalid argc (usage: `so_long MAP_FILE_NAME`)\n";
+
+#endif
