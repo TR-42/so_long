9320c5e7b94877f18e105c086c388b964c3549be
kfujita <kfujita@student.42tokyo.jp>
2023-04-10 06:43:01 +0900

    Create validate_input.h

---
+++ headers/validate_input.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate_input.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 05:26:35 by kfujita           #+#    #+#             */
+/*   Updated: 2023/04/10 05:43:49 by kfujita          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VALIDATE_INPUT_H
+# define VALIDATE_INPUT_H
+
+# include <stdbool.h>
+
+# include "so_long.h"
+
+// checks...
+// - col count
+// - invalid chars
+bool	is_valid_map_style(const t_so_long *d);
+
+// checks...
+// - is it closed/surrounded with wall?
+// - start/exit/collectable count
+bool	is_valid_map_data(const t_so_long *d);
+
+// checks...
+// - is it solvable?
+bool	is_map_solvable(const t_so_long *d);
+
+#endif
