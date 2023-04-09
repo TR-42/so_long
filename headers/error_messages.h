f8fd473784d03faa7ff7451c40d0edbfee9c1bda
kfujita <kfujita@student.42tokyo.jp>
2023-04-09 11:36:33 +0900

    `perror`のように扱えるような`print_error_msg2`関数を追加

--- headers/error_messages.h
+++ headers/error_messages.h
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 00:02:55 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/09 11:13:29 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/04/09 11:33:30 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ static const char	g_err_inval_extension[]
 static const char	g_err_inval_argc[]
 	= "Invalid argc (usage: `so_long MAP_FILE_NAME`)";
 
+int	print_error_msg2(const char *str1, const char *str2);
 int	print_error_msg(const char *str);
 int	print_error_errno(int num);
 int	print_error(void);
--- srcs/error_messages.c
+++ srcs/error_messages.c
@@ -6,7 +6,7 @@
 /*   By: kfujita <kfujita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 11:00:58 by kfujita           #+#    #+#             */
-/*   Updated: 2023/04/09 11:13:35 by kfujita          ###   ########.fr       */
+/*   Updated: 2023/04/09 11:35:51 by kfujita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,19 @@
 #include "ft_put/ft_put.h"
 #include "error_messages.h"
 
+int	print_error_msg2(const char *str1, const char *str2)
+{
+	write(STDERR_FILENO, g_err, sizeof(g_err) - 1);
+	ft_putstr_fd((char *)str1, STDERR_FILENO);
+	if (str1 != NULL && str2 != NULL)
+		write(STDERR_FILENO, ": ", 2);
+	ft_putendl_fd((char *)str2, STDERR_FILENO);
+	return (1);
+}
+
 int	print_error_msg(const char *str)
 {
-	write(STDERR_FILENO, g_err, sizeof(g_err));
-	ft_putendl_fd((char *)str, STDERR_FILENO);
-	return (1);
+	return (print_error_msg2(NULL, str));
 }
 
 int	print_error_errno(int num)
